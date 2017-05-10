a3c29b50603d8379ed1c4d8a57a7b47e5620da83
RobLukas <rjlukasik@wp.pl>
2017-05-10 19:05:34 +0200

    Basics CSharp

--- HelloWorldApp/HelloWorldApp/Program.cs
+++ HelloWorldApp/HelloWorldApp/Program.cs
@@ -2,7 +2,6 @@
 using static System.Console;
 using Introduction = Company.MyProject.Basics;
 
-
 namespace HelloWorldApp
 {
     class Program
@@ -15,9 +14,26 @@ namespace HelloWorldApp
         {
             public const string DayOfSandingBill = "Monday";
             public int CustomerID;
-            public string FirstName;
+            
             public string LastName;
+            private string _firstName;
+            public string FirstName
+            {
+                get { return _firstName; }
+                set { _firstName = value; }
+            }
+            private decimal _data;
+            public decimal Data
+            {
+                get { return _data; }
+                set { _data = value; }
+            }
 
+            public decimal DataAutoImplemented
+            {
+                get;
+                set;
+            }
         }
 
         public enum TimeOfDay
@@ -42,6 +58,7 @@ namespace HelloWorldApp
             Type isRabbitType = isRabbit.GetType();
 
             WriteLine($"name is type {nameType}");
+            WriteLine($"name is type {name.GetType()}");
             WriteLine($"age is type {ageType}");
             WriteLine($"isRabbit is type {isRabbitType}");
 
@@ -72,7 +89,7 @@ namespace HelloWorldApp
             WriteLine(jeby);
 
             int b = 2;
-            
+
 
             if (DoSomething(b) != 0)
             {
@@ -121,6 +138,54 @@ namespace HelloWorldApp
             {
                 WriteLine(args[i]);
             }
+            var customer1 = new PhoneCustomer();
+
+
+            WriteLine($"\n{customer1.GetType()}");
+            WriteLine($"{customer1.Data.GetType()}");
+            WriteLine($"{customer1.DataAutoImplemented}");
+
+            string message = "WITAM";
+            string upperMessage = message.ToLower();
+            WriteLine(upperMessage);
+
+            int xMessage = 2017;
+            message += xMessage.ToString();
+            WriteLine(message);
+
+
+            // C# 6.0 pl
+
+            int[,] matrix2D = new int[3,3];
+
+            int[,] rectangularMatrix =
+            {
+                {0, 1, 2 },
+                {0, 2, 3 },
+                {1, 2, 3 }
+            };
+
+            int[][] matrix = new int[][]
+            {
+                new int[] {3, 3, 3},
+                new int[] {2, 2, 2},
+                new int[] {1, 1, 1, 2}
+            };
+
+            var matrixVar = new int[][]
+            {
+                new int[] {3, 3, 3},
+                new int[] {2, 2, 2},
+                new int[] {1, 1, 1, 2}
+            };
+
+            var charArray = new[] { 'a', 'b', 'c' };
+
+            //różna długość tablicy wewn
+            int[][] matrix2 = new int[3][];
+
+
+
         }
     }
 }
